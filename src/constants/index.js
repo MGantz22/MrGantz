2265abf13539468486ed415795a9cae0d8294f1d
Mitchell <mgantz22@icloud.com>
2023-07-14 19:30:55 -0700

    delete old code, add opine text

--- src/constants/index.js
+++ src/constants/index.js
@@ -23,6 +23,7 @@ import {
   space,
   unity,
   Csharp,
+  Opine,
 
 
 } from "../assets";
@@ -40,15 +41,6 @@ export const navLinks = [
     id: "contact",
     title: "Contact",
   },
-  // {
-  //   id: "LinkedIn",
-  //   title: "LinkedIn",
-  //   link: "https://www.linkedin.com/in/mitchell-gantz/",
-  // },
-  // {
-  //   id: "github",
-  //   title: "GitHub",
-  // },
 ];
 
 const services = [
@@ -147,18 +139,33 @@ const experiences = [
     ],
   },
   {
-    title: "Student",
+    title: "Graduate",
     company_name: "Epicodus",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
+    date: "Jan 2023 - July 2023",
     points: [
       "Epicodus 2023 Tree Cohort",
       "A Coding bootcamp that emphasizes pair programming in a fast-paced, results-driven setting.",
       "The program offers instruction in key competencies such as C#, JavaScript, React, SQL, and .NET, with an emphasis on building a strong foundation.",
       "In addition to technical skills, the program also prioritizes Diversity, Equity, and Inclusion lessons to help students develop soft skills and become well-rounded team players.",
-      "Upon completion of the program, I will have gained skills in .NET Core, JavaScript, React, HTML, GitHub, C#, .NET Framework, Pair Programming, Cascading Style Sheets (CSS), Test Driven Development, Object-Oriented Programming (OOP), Responsive Web Design, and User Interface Design.",
-      "I Participate in code reviews and recieve constructive feedback on projects.",
+      "With completion of the program, I gained skills in .NET Core, JavaScript, React, HTML, GitHub, C#, .NET Framework, Pair Programming, Cascading Style Sheets (CSS), Test Driven Development, Object-Oriented Programming (OOP), Responsive Web Design, and User Interface Design.",
+      "I Participated in code reviews and recieved constructive feedback on projects weekly.",
+    ],
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Opine",
+    icon: Opine,
+    iconBg: "#E6DEDD",
+    date: "June 2023 - Present",
+    points: [
+      "Epicodus post grad internship (Remote)",
+      "Tasked with updating an ongoing project by implementing bug fixes and incorporating new features.",
+      "Actively participating in daily stand-up sessions to receive guidance and coaching from a senior developer.",
+      "Identifying potential issues and improvements for the current production phase of the project.",
+      "Utilizing Firestore, Firebase, JavaScript, and React.js to develop and enhance the project.",
+      "Prioritizing the delivery of user-friendly educational experiences.",
     ],
   }
 ];
@@ -170,7 +177,7 @@ const testimonials = [
     name: "Calvin Will",
     designation: "Instructor",
     company: "Epicodus",
-    image: "https://media.licdn.com/dms/image/C4E03AQHV81KPq_bTlg/profile-displayphoto-shrink_400_400/0/1581905259661?e=1687996800&v=beta&t=oyhQMh114VAGsDK15jUrUqGYkKym-EVpAa_KQaSm6Dk",
+    image: "https://media.licdn.com/dms/image/D5603AQHla7xO5hGz9w/profile-displayphoto-shrink_800_800/0/1688948134188?e=1694649600&v=beta&t=Ylq3jHqqWv4JagHeW5vuxWCL5UgZPrpg1Ci3lzYFb7o",
   },
   {
     testimonial:
@@ -202,7 +209,7 @@ const testimonials = [
     name: "Thomas Johnston",
     designation: "Superintendent",
     company: "DoveTail",
-    image: "https://media.licdn.com/dms/image/C5603AQGR8oxauiZKyg/profile-displayphoto-shrink_800_800/0/1520975478328?e=1687996800&v=beta&t=rMuMwsXMjff8pFHgHag6kuCT-lBLbetNKb8FdC-sodY",
+    image: "https://media.licdn.com/dms/image/C5603AQGR8oxauiZKyg/profile-displayphoto-shrink_200_200/0/1520975478328?e=1694649600&v=beta&t=zL9x9qyRhXFvsMV8mfBEaV3uoVUcvjvDiHov4sQLgsQ",
   }
 ];
 
