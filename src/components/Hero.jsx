3d863b61012f965bd53b1d1450180761ebc98677
Mitchell Gantz <mgantz22@icloud.com>
2024-03-19 19:53:06 -0700

    Remove Full stack from title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           Hi, I'm <span className='text-[#915EFF]'>Mitchell Gantz</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Full-Stack, Software, and Game Developer <br className='sm:block hidden' />
+          Software and Game Developer <br className='sm:block hidden' />
           </p>
         </div>
       </div>
